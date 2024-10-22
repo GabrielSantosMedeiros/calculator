64ab779fd50182915afeb57858d2f9fb36e05075
GabrielSantosMedeiros <gabriel7.gs61@gmail.com>
2024-10-22 16:28:27 -0300

    create empty main frame

--- src/main/java/com/portfolio/App.java
+++ src/main/java/com/portfolio/App.java
@@ -1,13 +1,14 @@
 package com.portfolio;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.JFrame;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        JFrame mainFrame = new JFrame("Calculator");
+        mainFrame.setSize(300, 400);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setDefaultCloseOperation(mainFrame.DISPOSE_ON_CLOSE);
+        mainFrame.setVisible(true);
     }
 }
